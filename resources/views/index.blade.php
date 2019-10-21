92a11394661924f05a2d0ac111c4f569487793a6
miccajr <junior.xrpl2.xv@gmail.com>
2019-10-21 15:27:34 +0700

    projek kedua

---
+++ resources/views/index.blade.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+
+    <title>Kendaraan Bermasalah!</title>
+  </head>
+
+  <body>
+  <nav>
+  <div class="container">
+            <ul>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#about">Tentang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#pesan">Keluhan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#video">Video</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#masalah">Masalah</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#home">Home</a>
+                </li>
+            </ul>
+  </div>
+</nav>
+    <div id="home">
+        <div class="jumbotron"><br>
+            <img src="/css/img/cargo.png" width="350" height="400">
+                <h2><p class="stroke"><b>INI MASALAHKU BERSAMA BANYAK PENGENDARA DI INDONESIA</b></p></h1>
+                <hr size="12px" class="my-4">
+                <p><b>Permasalahan memang bisa terjadi dimana-mana, Bahkan ada dalam kendaraan yang sering kita gunakan.</b></p>
+                <p><b>Untuk kalian yang suka menggunakan kendaraan pribadi, pasti ada keluh kesah dalam menggunakannya kan<br>
+                seperti harus berlama-lama mengantri bensin yang cukup panjang dan bisa banyak membuang waktu.</b></p>
+        </div>
+    </div>
+    <div id="masalah"><br><br><br><center>
+                <table width="900" height="350">
+                    <tr>
+                        <td><img src="/css/img/antribensin.png" height="250" width="300">
+                            <p><b>Suka lama untuk mengantri Bensin,<br> padahal harus on time datang ke tujuan?</b></p></td>
+                        <td><img src="/css/img/fuel.png" height="250" width="300">
+                            <p><b>Bensin sekarat banget,<br>tapi letak keberadaan Pom Bensin masih jauh?</b></p></td>
+                    </tr>
+                    <tr>
+                        <td><img src="/css/img/cargas.png" height="200" width="250">
+                        <p><b>Mengisi bensin yang sangat lama <br>bisa membuang waktu yang dapat digunakan <br>untuk kegiatan bermanfaat!</b></p></td>
+                        <td><img src="/css/img/diselakin.png" height="200" width="350">
+                        <p><b>Udah nunggu ngantri panjang, <br>tapi orang lain dengan seenaknya menyelak?</b></p></td>
+                    </tr>
+                </table>
+            </center>
+    </div>
+    <div id="video">
+            <div class="container"><br><br><br>
+                <h2><b>TONTON VIDEO INI, DAN BERIKAN TANGGAPAN MU!</b></h2>
+            <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="788.54" height="443" type="text/html" src="https://www.youtube.com/embed/FtKSl5Uq_98?autoplay=0&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=https://youtubeembedcode.com"><div><small><a href="https://youtubeembedcode.com/es/">youtubeembedcode es</a></small></div><div><small><a href="http://add-link-exchange.com">ww://add-link-exchange.com</a></small></div></iframe>
+                <br><br><b>Setelah kami selidiki, ternyata tidak sedikit orang mengalami hal yang sama. Kebanyakan pengendara harus mengantri untuk bisa<br>
+                mendapatkan bahan bakar, hal tersebut tidaklah efisien lantaran memakan waktu apalagi untuk kalian yang selalu ingin on time dengan kegiatannya.</b>
+            </div>
+    </div>
+    <div id="pesan">
+        <div class="container"><br><br><br><h2>
+                <p style="text-align:center;">PUNYA PROBLEM YANG SAMA? YUK ISI PERTANYAAN DI BAWAH INI.</p></h2>
+                    <form>
+                        <table width="950">
+                            <tr>
+                                <td>
+                            <div class="form-group">
+                              <label for="formGroupExampleInput">Nama :</label>
+                              <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Masukan Nama...">
+                            </div>
+                            <div class="form-group">
+                              <label for="exampleInputEmail1">Email :</label>
+                              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Email...">
+                            </div>Jenis Kelamin :
+                            <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="pria" value="option1" checked>
+                                    <label class="form-check-label" for="exampleRadios1">
+                                    Pria
+                                    </label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="wanita" value="option2">
+                                    <label class="form-check-label" for="exampleRadios2">
+                                    Wanita
+                                    </label><br><br>
+                            </div>
+                            <div class="form-group">
+                                    <label for="formGroupExampleInput">Seberapa sering mengisi bensin :</label>
+                                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Berapa kali...">
+                                  </div>
+                                        <div class="form-group">
+                                          <label for="exampleFormControlTextarea1">Ceritakan Permasalahan kamu disini :</label>
+                                          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                        </div>
+                            <button type="submit" class="btn btn-primary">Kirim</button>
+                                </td>
+                            </tr>
+                        </table>
+                          </form><br>
+            </div>
+        </div>
+        <div id="about">
+            <div class="container"><br><br><br>
+                            <h1><b>Tentang Kami</b></h1><hr>
+                            <h3>Kami sekumpulan Anak Bangsa yang mempunyai banyak impian untuk merubah Indonesia menjadi lebih baik.<br>
+                            <br>Dengan kemauan tekad yang tinggi, kami membangun sebuah karya bersama-sama untuk berguna bagi Nusa dan Bangsa.</h3>
+            </div>
+        </div>
+        <div class="footer">
+            <table align="center" width="1100">
+                <tr>
+                    <th colspan="2"><b>Kontak Kami</b></th>
+                    <th colspan="2"><b>Temukan Kami</b></th>
+                </tr>
+                <tr height="10">
+                    <td width="70">Email</td>
+                    <td>: bepomobile@gmail.com</td>
+                    <td><a href="https://www.facebook.com/Pompom-105781064181138/">Facebook</a></td>
+                </tr>
+                <tr>
+                    <td>No. HP</td>
+                    <td>: +62 812 8008 6173</td>
+                    <td>Instagram</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td><a href="https://www.youtube.com/channel/UCh0gmmshK3Bx8zfsr0yYGTA">Youtube</a></td>
+                </tr>
+            </table>
+                <p align="center">Copyright &copy; Bepo2019</p>
+             </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
